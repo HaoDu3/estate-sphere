3e54561e0a10fde7d3788650c60b093d956b782a
Hao Du <142969898+HaoDu3@users.noreply.github.com>
2024-06-05 02:19:03 -0400

    Add files via upload

---
+++ pages/Register.js
@@ -0,0 +1,67 @@
+
+import axios from "axios";
+import {useState} from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+     const {data} = await axios.post('pre-signup', {
+       email,
+       password
+     });
+     if(data?.error) {
+        toast.error(data.error);
+        setLoading(false);
+        return;
+      }
+      toast.success('Please check your email to complete registration');
+      setLoading(false);
+     console.log(data);
+     navigate('/');
+    } catch (error) {
+      console.log(error)
+      toast.error('Something went wrong');
+      setLoading(false);
+    }
+  }
+  return (
+    <div>
+      <h1 className="display-1 bg-primary text-light p-5">Register</h1>
+      <div
+        className="container"
+      >
+        <div className="row">
+          <div className="col-lg-4 offset-lg-4">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value = {email} onChange={(e) => {
+                  setEmail(e.target.value);
+                }} />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+              </div>
+              <button disabled= {loading} type="submit" className="btn btn-primary">
+                {loading ? 'Loading...': 'Register'}
+              </button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
---
+++ pages/auth/AccountActivates.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import {useAuth} from '../../context/auth';
+
+
+export default function AccountActivate() {
+    const [auth, setAuth] = useAuth();
+
+    const { token } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(token) requestActication();
+    }, [token]);
+
+    const requestActication = async () => {
+        try {
+            const {data} = await axios.post(`/signup`, { token });
+            if(data?.error) return toast.error(data.error);
+            localStorage.setItem('auth', JSON.stringify(data));
+            console.log('Account activation');
+            setAuth(data);
+            toast.success("Successfully activated account.");
+            navigate('/');
+        } catch (error) {
+            console.log('Account activation error', error);
+            toast.error(error.response.data.error);
+        }
+    };
+
+    console.log (token);
+    return (
+      <div className="display-1 d-flex justify-content-center align-items-center-vh-100">
+        Please Wait...
+        </div>
+    );
+}
