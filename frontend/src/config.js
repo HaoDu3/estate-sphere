724562f3b44e66ef9139611d48f4adbed6bcf580
Hao Du <142969898+HaoDu3@users.noreply.github.com>
2024-06-05 22:24:49 -0400

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 
 export const API = 'http://localhost:8000/api';
 export const GOOGLE_PLACES_KEY ="AIzaSyC3-1nLm9JyUKIYsTmpSIFjh_hIiebSYqQ";
-export const GOOGLE_MAP_KEY ="AIzaSyDwiJiZcVl8SA_vvX_TkTkkuLza1UC3trk"
+export const GOOGLE_MAP_KEY ="AIzaSyDwiJiZcVl8SA_vvX_TkTkkuLza1UC3trk";
