69317fb77f414d42bb4fc27374ed22f1d87cf22b
Hao Du <142969898+HaoDu3@users.noreply.github.com>
2024-06-10 14:30:04 -0400

    Backend and fornend

--- frontend/src/config.js
+++
@@ -1,4 +0,0 @@
-
-export const API = 'http://localhost:8000/api';
-export const GOOGLE_PLACES_KEY ="AIzaSyC3-1nLm9JyUKIYsTmpSIFjh_hIiebSYqQ";
-export const GOOGLE_MAP_KEY ="AIzaSyDwiJiZcVl8SA_vvX_TkTkkuLza1UC3trk";
